5a8cccd833063a68a2313cb5d5780fbceaf758b5
“jhirsch1” <jacob_hirschhorn@brown.edu>
2025-06-17 17:08:37 -0400

    added password

--- src/App.tsx
+++ src/App.tsx
@@ -21,23 +21,37 @@ function App() {
         skipEmptyLines: true,
       }).data as Array<Record<string, string>>);
 
-    console.log(peoplelist[17]);
-    console.log(peoplelist[17].info.includes("\n"));
+    // console.log(peoplelist[17]);
+    // console.log(peoplelist[17].info.includes("\n"));
 
-    const [tree, setTree] = useState("Hirschhorn");
+    // Password box
+    const [textbox, setTextbox] = useState("");
+    const [open, setOpen] = useState(false);
+    console.log(open)
 
-    function handleClickHirschhorn() {
-        // Handle click event
-        console.log("Clicked on Hirschhorn");
-        // setClicked(!clicked); // Toggle clicked state
-        setTree("Hirschhorn");
+    function handleSubmit() {
+        if (textbox === "Password") {
+            setOpen(true)
+        }
+        console.log(open)
     }
-    function handleClickBronsther() {
-        // Handle click event
-        console.log("Clicked on Bronsther");
-        // setClicked(!clicked); // Toggle clicked state
-        setTree("Bronsther");
-    }
+
+
+    // Family of the family tree - not in use now, maybe in the future
+    // const [tree, setTree] = useState("Hirschhorn");
+
+    // function handleClickHirschhorn() {
+    //     // Handle click event
+    //     console.log("Clicked on Hirschhorn");
+    //     // setClicked(!clicked); // Toggle clicked state
+    //     setTree("Hirschhorn");
+    // }
+    // function handleClickBronsther() {
+    //     // Handle click event
+    //     console.log("Clicked on Bronsther");
+    //     // setClicked(!clicked); // Toggle clicked state
+    //     setTree("Bronsther");
+    // }
 
 
     return (
@@ -47,36 +61,47 @@ function App() {
         <h1>Family Tree</h1>
         <p>Click on a person to see their details.</p>
 
-        {peoplelist.map((person, index) => (
-          <Person
-            name={person.name}
-            birth={
-              person.birth
-            }
-            death={
-              person.death
-            }
-            marriage_date={
-              person.marriage_date
-            }
-            father={person.father}
-            mother={person.mother}
-            spouse={person.spouse}
-            children={person.children}
-            info={person.info}
-            birth_place={person.birth_place}
-            death_place={person.death_place}
-            marriage_place={person.marriage_place}
-            generation={person.generation}
-            spouse2={person.spouse2}
-            marriage_place2={person.marriage_place2}
-            marriage_date2={
-              person.marriage_date2
-            }
-            children2={person.children2}
-            index={person.index}
+        {!open ? (
+          <input
+            type="text"
+            aria-label="Password input box"
+            aria-description="Type password in this input box"
+            className="PasswordBox"
+            placeholder="Enter Password"
+            onChange={(e) => setTextbox(e.target.value)}
+            value={textbox}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                handleSubmit();
+              }
+            }}
           />
-        ))}
+        ) : null}
+
+        {open
+          ? peoplelist.map((person, index) => (
+              <Person
+                name={person.name}
+                birth={person.birth}
+                death={person.death}
+                marriage_date={person.marriage_date}
+                father={person.father}
+                mother={person.mother}
+                spouse={person.spouse}
+                children={person.children}
+                info={person.info}
+                birth_place={person.birth_place}
+                death_place={person.death_place}
+                marriage_place={person.marriage_place}
+                generation={person.generation}
+                spouse2={person.spouse2}
+                marriage_place2={person.marriage_place2}
+                marriage_date2={person.marriage_date2}
+                children2={person.children2}
+                index={person.index}
+              />
+            ))
+          : null}
       </div>
     );
 }
